87073dba924d66085069720b0a4d57fa4ed36b09
Dharshana Jayamaha <dharshanajayamaha@yahoo.com>
2014-08-20 22:51:34 +0530

    some

--- src/application/core/MY_Model.php
+++ src/application/core/MY_Model.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ * @author     Dharshana Jayamaha <me@geewantha.com>
+ * @copyright  2013 Dharshana Jayamaha
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    Release: 1.0
+ * @link       http://pear.php.net/package/PackageName
+ * @since      Class available since Release 1.0.0
+ */
 class MY_Model extends CI_Model
 {
     protected $_table_name = '';
--- src/application/models/page_m.php
+++ src/application/models/page_m.php
@@ -3,8 +3,8 @@
  * Page Model
  *
  * Long description for class (if any)...
- * @author     Dharshana Jayamaha <dharshana@openarc.lk>
- * @copyright  2013 RAD Team - OpenArc
+ * @author     Dharshana Jayamaha <me@geewantha.com>
+ * @copyright  2014 Dharshana Jayamaha
  * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    Release: 1.0
  * @link       http://pear.php.net/package/PackageName
